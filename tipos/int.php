99c916dce6763e398ff9e4592dc2563c15f52c1a
GuiGodooi <gui.go.d@hotmail.com>
2023-09-15 16:17:58 -0300

    Tipos - Inteiros

--- tipos/int.php
+++ tipos/int.php
@@ -6,6 +6,12 @@ echo '<br>';
 
 /* Informando sobre a variável */
 var_dump(11);
+echo '<br>';
 
-/* Empnho máximo - valor suportado */
-echo PHP_INT_MAX;
+/* EmpEnho máximo e mínimo - valor extremos suportados */
+echo PHP_INT_MAX , '<br>';
+echo PHP_INT_MIN , '<br>';
+
+echo 011, '<br>'; // Base Octal
+echo 0b11000111001, '<br>'; // Base Binária
+echo 0x117acf0; // Base Hexa
