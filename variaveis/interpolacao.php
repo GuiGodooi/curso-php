5f0e0e4586c50107e69d23c6c66541dc101624ba
GuiGodooi <gui.go.d@hotmail.com>
2023-09-18 13:31:07 -0300

    Interpolação

--- variaveis/interpolacao.php
+++ variaveis/interpolacao.php
@@ -1,3 +1,15 @@
 <div class="titulo">Interpolação</div>
 
 <?php
+$numero = 10;
+echo $numero;
+echo '<br> $numero'; // não interpola de forma alguma
+echo "<br> $numero"; 
+
+$texto = "A sua nota é: $numero";
+echo "<br>$texto";
+
+$objeto = 'caneta';
+echo "<br>Eu tenho 5 $objeto"; // Aqui sim, ele vai interpolar
+echo "<br>Eu tenho 5 {$objeto}s.";
+echo "<br>Eu tinha 5 { $objeto}s mas perdi 3 {$objeto }s";
