e83b8853026a4f045bbe3acce18d55cd39c69049
GuiGodooi <gui.go.d@hotmail.com>
2023-09-18 10:32:09 -0300

    Variaveis #1

--- variaveis/basico.php
+++ variaveis/basico.php
@@ -1,1 +1,22 @@
 <div class="titulo">Variáveis</div>
+
+<?php
+// Utilizamos $ para guaradar a variável
+$numeroA = 13;
+echo $numeroA, '<br>';
+var_dump($numeroA);
+
+echo '<br>';
+$a = 3;
+$b = 16;
+$soma = $a + $b;
+
+echo 'O Resultado é: ' . $soma;
+echo '<br>';
+echo isset($soma);
+
+$variavel = 10; //tipagem fraco, dinamica
+echo '<br>' . $variavel;
+
+$variavel = "Agora sou uma string!";
+echo '<br>' . $variavel;
