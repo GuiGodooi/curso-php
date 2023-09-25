938081d343466d19e29bf20dd65d9696db6f756e
GuiGodooi <gui.go.d@hotmail.com>
2023-09-25 10:02:43 -0300

    If / Else

--- controle/if_else.php
+++ controle/if_else.php
@@ -1,3 +1,20 @@
 <div class="titulo">If Else</div>
 
 <?php
+/*
+Nesta caso (true) ele irá imprimir os 2 textos,
+agora se utilizamos false, ele imprime apenas o ultimo:
+
+if(true)
+    echo "Serei impresso?";
+    echo "Serei impresso novamente?"; <- Apenas esse será impresso
+*/
+
+if(true)
+    echo "Serei impresso? ";
+    echo "Serei impresso novamente?<br>";
+
+if(true){
+    echo "Serei impresso?<br>";
+    echo "Serei impresso novamente?";
+}
