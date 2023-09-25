63b5091e62259c26c8b9d873306bc0ec6b79b15b
GuiGodooi <gui.go.d@hotmail.com>
2023-09-25 11:25:01 -0300

    If else - Parte 2

--- controle/if_else.php
+++ controle/if_else.php
@@ -14,7 +14,28 @@ if(true)
     echo "Serei impresso? ";
     echo "Serei impresso novamente?<br>";
 
+
 if(true){
     echo "Serei impresso?<br>";
-    echo "Serei impresso novamente?";
+    echo "Serei impresso novamente?<br>";
+};
+
+if(false){
+    echo "Verdadeiro - Parte A<br>";
+    echo "Verdadeiro - Parte B<br>";
+} else{
+    echo "Falso - Parte A<br>";
+    echo "Falso - Parte B<br>";
 }
+
+if(false){
+    echo "Passo A<br>";
+} else if(false){
+    echo "Passo B<br>";
+} elseif(true){
+    echo "Passo C<br>";
+} else {
+    echo "Último Passo<br>";
+};
+
+echo "Fim<br>";
