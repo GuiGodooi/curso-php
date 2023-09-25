4ad2319eb6d2d64838f9cf4a14182af7f198ef6b
GuiGodooi <gui.go.d@hotmail.com>
2023-09-25 12:01:52 -0300

    Operadores Relacionais

---
+++ controle/operadores_relacionais.php
@@ -0,0 +1,63 @@
+<div class="titulo">Operadores Relacionais</div>
+
+<?php
+echo "1 == 1?<br>";
+var_dump(1 == 1);
+echo '<br>';
+echo "1 > 1?<br>";
+var_dump(1 > 1);
+echo '<br>';
+echo "1 >= 1?<br>";
+var_dump(1 >= 1);
+echo '<br>';
+echo "4 < 23?<br>";
+var_dump(4 < 23);
+echo '<br>';
+echo "1 <= 7?<br>";
+var_dump(1 <= 7);
+echo '<br>';
+echo "1 != 1?<br>";
+var_dump(1 != 1);
+echo '<br>';
+echo "1 <> 1?<br>";
+var_dump(1 <> 1);
+echo '<br>';
+
+var_dump(111 == '111'); // são iguais = True
+var_dump(111 === '111'); // não são estritamente iguais = false
+var_dump(111 != "111"); // não são diferentes = false
+var_dump(111 !== '111'); // são estritamente diferentes = true
+echo '<br>';
+
+echo "<p>Relacionais no IF/ELSE</p><hr>";
+$idade = 75;
+if($idade < 18){
+    echo "Menor de Idade = $idade anos!<br>";
+} else if($idade < 65){
+    echo "Adulto = $idade anos!<br>";
+} else{
+    echo "Terceira Idade = $idade anos!";
+}
+
+echo '<p>Spaceship</p><hr>';
+var_dump(500 <=> 3);
+var_dump(50 <=> 50);
+var_dump(5 <=> 50);
+
+
+echo '<p>Valores podem ser Verdadeiro ou Falso</p><hr>';
+var_dump(!!5); //dupla negação;
+var_dump(!!0);
+var_dump(!!"");
+var_dump(!!" ");
+?>
+
+<style>
+    p{
+        margin-bottom: 0px;
+    }
+
+    hr{
+        margin-top: 0px;
+    }
+</style>
