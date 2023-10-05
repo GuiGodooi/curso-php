0771e7fa8c7747841e424926704507698bead389
GuiGodooi <gui.go.d@hotmail.com>
2023-10-05 18:02:43 -0300

    Desafio for concluido

--- repeticoes/desafio_for.php
+++ repeticoes/desafio_for.php
@@ -1,1 +1,30 @@
 <div class="titulo">Desafio FOR</div>
+
+<!--
+    Usar o for...
+    #
+    ##
+    ###
+    ####
+    #####
+    1)Pode usar incremento $i++;
+    2)Não pode usar incremento $i++;
+-->
+
+<?php
+$imp = '';
+for($cont = 1; $cont <= 5; $cont++){
+    $imp .= '#';
+    echo "$imp <br>";
+}
+
+echo "<hr>";
+
+for(
+    $imp2 = '#';
+    $imp2 !== '######';
+    $imp2 .= '#'
+    )
+    {
+    echo "$imp2 <br>";
+}
