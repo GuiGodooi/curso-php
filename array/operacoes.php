097976f305f874fa36cb8fe653266308b8fdca6f
GuiGodooi <gui.go.d@hotmail.com>
2023-10-05 14:18:26 -0300

    operações

--- array/operacoes.php
+++ array/operacoes.php
@@ -3,7 +3,7 @@
 <?php
 $dados1 = [
     "nome" => "Gui",
-    "idade" => "24",
+    "idade" => 24
 ];
 
 $dados2 = [
@@ -20,4 +20,29 @@ echo '<br>';
 $indice = array_rand($dadosCompletos);
 echo "$indece = $dadosCompletos[$indice]";
 echo '<br>';
-echo "$dadosCompletos[$idade]";
+echo "{$dadosCompletos['idade']}";
+echo " ${dadosCompletos['idade']}";
+
+//serve para liberar uma memória
+unset($dadosCompletos["nome"]);
+echo '<br>';
+print_r($dadosCompletos);
+
+unset($dadosCompletos);
+echo '<br>';
+//var_dump($dadosCompletos);
+
+$impares = [1, 3, 5, 7, 9];
+$pares = [0, 2, 4, 6, 8];
+
+$decimais = $pares + $impares;
+echo '<br>';
+print_r($decimais);
+
+$decimais = array_merge($pares, $impares);
+echo '<br>';
+print_r($decimais);
+
+sort($decimais);
+echo '<br';
+print_r($decimais);
