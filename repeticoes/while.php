a4e25a5e1087ea968aa8e105c3a90f2ccca649ef
GuiGodooi <gui.go.d@hotmail.com>
2023-10-11 10:55:32 -0300

    Do While

--- repeticoes/while.php
+++ repeticoes/while.php
@@ -13,3 +13,20 @@ while($contador < VALOR_LIMITE){
 // echo "while $contador <br>";
 //  $contador++;
 // } while ($contador < VALOR_LIMITE);
+
+//for($contador = 0; $contador < VALOR_LIMITE; $contador++) {
+//    echo "for $contador <br>";
+//}
+
+$contador = 0;
+do{
+    echo "Do-While $contador <br>";
+    $contador++;
+}while($contador < VALOR_LIMITE);
+
+$contador = 0;
+while(true){
+   echo "While(true) $contador <br>";
+   $contador++;
+   if($contador >= VALOR_LIMITE) break; 
+}
