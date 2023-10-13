c523301a6da981ddc66adb8c9e5e136acfc87d5d
GuiGodooi <gui.go.d@hotmail.com>
2023-10-13 11:17:40 -0300

    Escopo

--- funcoes/escopo.php
+++ funcoes/escopo.php
@@ -1,2 +1,36 @@
 <div class="titulo">Escopo</div>
 
+<?php
+function imprimirMensagem(){
+    echo "Olá! ";
+    echo "Até a próxima!<br>";
+}
+
+imprimirMensagem();
+imprimirMensagem();
+imprimirMensagem();
+
+$variavel = 1;
+
+function trocarValor(){
+    $variavel = 2;
+    echo "Durante a função: $variavel <br>";
+}
+
+echo "Antes: $variavel <br>";
+trocarValor();
+echo "Depois: $variavel <br>";
+
+function trocaValorDeVerdade(){
+    /*Acessa a variável de fora no caso => 1 */
+    global $variavel;
+    $variavel = 3;
+    echo "Durante a função: $variavel<br>";
+}
+
+echo "Antes: $variavel<br>";
+trocaValorDeVerdade();
+echo "Depois: $variavel <br>";
+
+//Retorna um valor vazio => NULL;
+var_dump(trocaValorDeVerdade());
